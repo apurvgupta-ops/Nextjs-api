f82e42bb0349e7c3a736cfff10cbff5da758a29b
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2022-02-17 22:53:57 +0530

    Rest api GET, PUT, DELETE

---
+++ pages/api/hero/[id].js
@@ -0,0 +1,51 @@
+import dbConnect from "../../../Db/dbConnect";
+import Hero from "../../../models/Hero";
+
+export default async (req, res) => {
+  const {
+    query: { id },
+    method,
+  } = req;
+  switch (method) {
+    case "GET":
+      try {
+        const hero = await Hero.findById(id);
+        if (!hero) {
+          res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, hero: hero });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "PUT":
+      try {
+        const hero = await Hero.findByIdAndUpdate(id, req.body, {
+          new: true,
+          revalidator: true,
+        });
+        if (!hero) {
+          res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, hero: hero });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
+    case "DELETE":
+      try {
+        const hero = await Hero.deleteOne({ _id: id });
+        if (!hero) {
+          res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, hero: hero });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+};
